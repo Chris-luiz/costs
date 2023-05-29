d0b1eb758cff705ccfac1e5c468ced7d98a4e868
Chris Pae <clspae@uea.edu.br>
2023-05-29 15:14:29 -0400

    refactor: remocao de rotas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-/*** IMPORTAÇÃO DE COMPONENTES ***/
 import Home from './components/pages/Home'
-import Contact from './components/pages/Contact'
-import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
 import Projects from './components/pages/Projects'
 import Project from './components/pages/Project'
 
-/*** IMPORTAÇÃO DE LAYOUTS ***/
 import Container from './components/layout/Container'
 import NavBar from './components/layout/NavBar'
 import Footer from './components/layout/Footer'
-
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
@@ -22,8 +17,6 @@ function App() {
         <Routes>
           <Route path='/'           element={<Home/>}       />
           <Route path='/projects'   element={<Projects/>}   />
-          <Route path='/company'    element={<Company/>}    />
-          <Route path='/contact'    element={<Contact/>}    />
           <Route path='/newproject' element={<NewProject/>} />
           <Route path='/project/:id' element={<Project/>} />
         </Routes>
