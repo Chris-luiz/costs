6c5f0227f130c65121e5c1580aa43cfc193c975c
Chris Pae <clspae@uea.edu.br>
2023-06-02 12:42:53 -0400

    fix: correcao de rotas

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Container from './components/layout/Container'
 import NavBar from './components/layout/NavBar'
 import Footer from './components/layout/Footer'
 
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
@@ -15,10 +15,11 @@ function App() {
       <NavBar />
       <Container customClass="min-height">
         <Routes>
-          <Route path='/'           element={<Home/>}       />
-          <Route path='/projects'   element={<Projects/>}   />
-          <Route path='/newproject' element={<NewProject/>} />
-          <Route path='/project/:id' element={<Project/>} />
+          <Route path='/costs' element={<Home />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/newproject' element={<NewProject />} />
+          <Route path='/project/:id' element={<Project />} />
         </Routes>
       </Container>
       <Footer />
