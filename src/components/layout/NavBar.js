8810d750b81aaaeaea16f7a5f2004cbce7dbddbb
Chris Pae <clspae@uea.edu.br>
2023-06-02 09:39:37 -0400

    refactor: navbar estilizada com tailwind

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,25 +1,22 @@
 import { Link } from 'react-router-dom'
-
 import Container from './Container'
-
-import styles from './NavBar.module.css'
 import logo from '../../img/costs_logo.png'
 
-function NavBar() {
+const NavBar = () => {
     return (
-        <nav className={styles.navbar}>
-            <Container>
+        <nav className='bg-zinc-900 p-2'>
+            <Container customClass='container'>
                 <Link to="/">
                     <img src={logo} alt="Logo" />
                 </Link>
-                <ul className={styles.list}>
-                    <li className={styles.item}>
+                <ul className='flex items-center'>
+                    <li className='text-white mr-4 hover:text-yellow-600'>
                         <Link to="/">Home</Link>
                     </li>
-                    <li className={styles.item}>
+                    <li className='text-white mr-4 hover:text-yellow-600'>
                         <Link to="/projects">Projetos</Link>
                     </li>
-                    <li className={styles.item}>
+                    <li className='text-white mr-4 hover:text-yellow-600'>
                         <Link to="/newproject">Novo Projeto</Link>
                     </li>
                 </ul>
